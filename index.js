b6d40213a26146d9d6f9e9680e2ce5258192cdeb
farisjamaan <faresjamaan@gmail.com>
2023-07-10 22:56:54 +0300

    added score keeping functionality

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ buttons.forEach(btn => {
 
 const round_selection = document.querySelector(".round_selection");
 const match_results = document.querySelector(".match_results");
+const scores = document.querySelector(".scores");
 
 
 // Function for the computer choice
@@ -15,6 +16,10 @@ function compSelection () {
     const output = Math.floor(Math.random() * choices.length);
     return choices[output];
 }
+
+playerCounter = 0;
+computerCounter = 0;
+
 
 // Function for a game round
 function playRound (selection) {
@@ -26,16 +31,34 @@ function playRound (selection) {
         match_results.textContent = "It's a TIE";
     } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
         match_results.textContent = 'Computer Wins';
+        computerCounter++
+        scores.textContent = `You: ${playerCounter}
+        Computer: ${computerCounter}`;
     } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
         match_results.textContent = 'You WON';
+        playerCounter++
+        scores.textContent = `You: ${playerCounter}
+        Computer: ${computerCounter}`;
     } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
         match_results.textContent = 'You WON';
+        playerCounter++
+        scores.textContent = `You: ${playerCounter}
+        Computer: ${computerCounter}`;
     } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
         match_results.textContent = 'Computer Wins';
+        computerCounter++
+        scores.textContent = `You: ${playerCounter}
+        Computer: ${computerCounter}`;
     } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
         match_results.textContent = 'Computer Wins';
+        computerCounter++
+        scores.textContent = `You: ${playerCounter}
+        Computer: ${computerCounter}`;
     } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
         match_results.textContent = 'You WON';
+        playerCounter++
+        scores.textContent = `You: ${playerCounter}
+        Computer: ${computerCounter}`;
     }
 }
 
